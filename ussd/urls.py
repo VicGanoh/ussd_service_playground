972728106234a53899e6c12f2afa44d819e764ec
VicGanoh <kofiganoh@gmail.com>
2024-10-16 08:30:46 -0700

    add url path

--- ussd/urls.py
+++ ussd/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import include, path
+from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("index/", views.index, name="index")
+]
