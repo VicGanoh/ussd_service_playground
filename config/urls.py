2001d89dfd5c44f520982a43d4ac9b81c40cf162
VicGanoh <kofiganoh@gmail.com>
2024-10-17 02:16:25 -0700

    install restframework package

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ussd.urls')),
+    path("admin/", admin.site.urls),
+    path("ussd-app/", include("ussd.urls")),
+    path("api-auth/", include("rest_framework.urls"))
 ]
 
 
--- ussd/views.py
+++ ussd/views.py
@@ -5,50 +5,55 @@ import logging
 logger = logging.getLogger(__name__)
 
 @csrf_exempt
-def index(request):
+def ussd_app(request):
 
     if request == "POST":
-        session_id = request.POST.get("sessionId")
-        service_code = request.POST.get("serviceCode")
-        phone_number = request.POST.get("phoneNumber")
-        text = request.POST.get("text")
+        session_id = request.POST.get("sessionID")
+        user_id = request.POST.get("userID")
+        new_session = request.POST.get("newSession")
+        phone_number = request.POST.get("msisdn")
+        network_provider = request.POST.get("network")
+        user_data = request.POST.get("userData")
 
         logging.info("session id %s", session_id)
-        logging.info("service code %s", service_code)
+        logging.info("user id %s", user_id)
         logging.info("phone_number %s", phone_number)
-        logging.info("text %s", text)
+        logging.info("user_data %s", user_data)
+        logging.info("new_session %s", new_session)
+        logging.info("network_provider %s", network_provider)
 
 
         response: str = ""
 
-        if text == "":
-            response = "CON Welcome to THE POLL \nPlease select an option"
+        if user_data == "":
+            response = "Welcome to THE POLL \nPlease select an option"
             response += "1. View Poll"
             response += "2.Vote"
             response += "3. View Results"
-        elif text == "1":
-            response = "END Poll Options"
+        elif user_data == "1":
+            response = "Poll Options"
             response += "1. John Paul"
             response += "2. Thomas Nketia"
             response += "3. Henry Baal"
-        elif text == "2":
+        elif user_data == "2":
             response = " Choose your candidate"
             response += "1. John Paul"
             response += "2. Thomas Nketia"
             response += "3. Henry Baal"
-        elif text == "2*1":
-            response = "END Successfully voted for John Paul"
-        elif text == "2*2":
-            response = "END Successfully voted for Thomas Nketia"
-        elif text == "2*3":
-            response = "END Successfully voted for Henry Baal"
-        elif text == "3":
-            response = "END Poll Results"
+        elif user_data == "2*1":
+            response = "Successfully voted for John Paul"
+        elif user_data == "2*2":
+            response = "Successfully voted for Thomas Nketia"
+        elif user_data == "2*3":
+            response = "Successfully voted for Henry Baal"
+        elif user_data == "3":
+            response = "Poll Results"
             response += "1. John Paul - 50%"
             response += "2. Thomas Nketia - 30%"
             response += "3. Henry Baal - 20%"
             response += "Thank you."
         
         return HttpResponse(response)
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 
